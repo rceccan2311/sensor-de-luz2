ac8990c84ba49994f7c672569ac243c830d7f4fa
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2024-02-12 09:51:24 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+basic.forever(function () {
+    if (input.lightLevel() < 50) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() < 75) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() < 90) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    }
+})
